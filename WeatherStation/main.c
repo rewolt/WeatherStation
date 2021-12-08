44e572fade8b7da68f2877f3bb9427fc9531b3d4
Bartłomiej Tomczak <rewolt90@gmail.com>
2021-12-08 22:45:57 +0100

    Added button to raise interrupt (on INT0). This will be main wake-up signal to show data once.

--- WeatherStation/main.c
+++ WeatherStation/main.c
@@ -10,6 +10,7 @@
 
 #include "main.h"
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdlib.h>
 #include "TWI/TWI.h"
@@ -22,11 +23,13 @@ char humidString[HUMID_STR_LEN];
 
 uint8_t thermometer[] = {0, 0, 0, 0, 0, 248, 12, 134, 134, 12, 248, 0, 0, 0, 0, 0, 0, 0, 0, 62, 99, 65, 156, 191, 191, 156, 65, 99, 62, 0, 0, 0};
 uint8_t cloud_drizzle[] = {192, 32, 16, 16, 24, 4, 2, 2, 2, 4, 8, 48, 64, 64, 64, 128, 0, 97, 26, 2, 194, 50, 2, 2, 98, 26, 2, 194, 50, 2, 2, 1};
+char test[] = {'1','2','3','4','5'};
 
 int main(void)
 {
 	TWI_Setup();
-	SSD1306_Init();	
+	SSD1306_Init();
+	INT0_Init();
 	
     while (1) 
     {
@@ -106,6 +109,20 @@ void HumidToString(double humidity, char *humidString, uint8_t strLength)
 	}
 	
 	humidString[strLength - 1] = (char)37;	// add %
+}
+
+void INT0_Init()
+{
+	GICR = (1 << INT0);
+	sei();
+}
+
+ISR(INT0_vect)
+{
+	SSD1306_ClearScreen();
+	SSD1306_PrintString(test, 5, 3);
+	_delay_ms(5000);
+	SSD1306_ClearScreen();
 }
 
 
--- WeatherStation/main.h
+++ WeatherStation/main.h
@@ -4,7 +4,7 @@
 # define F_CPU 8000000UL
 #include <avr/io.h>
 
-
+void INT0_Init();
 double CalculateTemp(uint8_t *packet);
 double CalculateHumidity(uint8_t *packet);
 void TempToString(double temperature, char *tempString, uint8_t strLength);
